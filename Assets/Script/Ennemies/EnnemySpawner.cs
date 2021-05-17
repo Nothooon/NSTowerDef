6890a0d46a7c933daa5c305f7f729021bfdb4ffe
BrettBeck <brett.becker35131@gmail.com>
2021-05-17 12:01:08 +0200

    intégration ennemis, modificaiton spawn
    intégration de 3 nouveaux ennemis et remaniement du système de spawn des ennemis

--- Assets/Script/Ennemies/EnnemySpawner.cs
+++ Assets/Script/Ennemies/EnnemySpawner.cs
@@ -9,7 +9,8 @@ public class EnnemySpawner : MonoBehaviour
     public GameObject instance;
     public Transform[] waypoints;
 
-    private int points;
+    private int points; // the higher the number, the more enemies will spawn
+    private float difficulty; // the higher the number, the lower is the difficulty
 
 
     void Start(){
@@ -21,44 +22,78 @@ public class EnnemySpawner : MonoBehaviour
         // WAVE 1
         points = 10;
         yield return new WaitForSecondsRealtime(5); // preparation time
+        difficulty = 0.5f;
         while(points > 0){ 
-            points -= SpawnEnnemies(points);
-            yield return new WaitForSecondsRealtime(1);
+            points -= SpawnEnnemies(points,difficulty);
+            yield return new WaitForSecondsRealtime(1); // interval between enemies spawn
         }
 
         // WAVE 2
-        points = 10;
+        points = 20;
         yield return new WaitForSecondsRealtime(5); // preparation time
+        difficulty = 0.2f;
         while(points > 0){ 
-            points -= SpawnEnnemies(points);
-            yield return new WaitForSecondsRealtime(0.5f);
+            points -= SpawnEnnemies(points,difficulty);
+            yield return new WaitForSecondsRealtime(1); // interval between enemies spawn
         }
 
         // WAVE 3
-        points = 20;
+        points = 1000;
         yield return new WaitForSecondsRealtime(5); // preparation time
+        difficulty = 0f;
         while(points > 0){ 
-            points -= SpawnEnnemies(points);
-            yield return new WaitForSecondsRealtime(0.25f);
+            points -= SpawnEnnemies(points,difficulty);
+            yield return new WaitForSecondsRealtime(1); // interval between enemies spawn
         }
         
     }
-    int SpawnEnnemies(int points){
+    int SpawnEnnemies(int points, float difficulty){
         float random = Random.Range(0f,1f);
-        if(random > 0){
-            instance = Instantiate(spawnee[0], spawnPoint.position, spawnPoint.rotation);
-            FollowThePath follow = instance.GetComponent(typeof(FollowThePath)) as FollowThePath;
-            instance.name = "Ennemy";
-            follow.SetWaypoints(waypoints);
-            return instance.GetComponent<Ennemy>().points;
-        }else{
-            return 0;
-        }        
+        random += difficulty;
+        if(random < 0.1){
+            if(((points -= spawnee[3].GetComponent<Ennemy>().points) >=0)){
+                instance = Instantiate(spawnee[3], spawnPoint.position, spawnPoint.rotation);
+                FollowThePath follow = instance.GetComponent(typeof(FollowThePath)) as FollowThePath;
+                //instance.name = "Ennemy";
+                follow.SetWaypoints(waypoints);
+                return spawnee[3].GetComponent<Ennemy>().points;
+            }else{
+                random += 0.1f;
+            }
+        }
+        if(random < 0.2){
+            if(((points -= spawnee[2].GetComponent<Ennemy>().points) >=0)){
+                instance = Instantiate(spawnee[2], spawnPoint.position, spawnPoint.rotation);
+                FollowThePath follow = instance.GetComponent(typeof(FollowThePath)) as FollowThePath;
+                //instance.name = "Ennemy";
+                follow.SetWaypoints(waypoints);
+                return spawnee[2].GetComponent<Ennemy>().points;
+            }else{
+                random += 0.2f;
+            }
+        }
+        if(random < 0.5){
+            if(((points -= spawnee[1].GetComponent<Ennemy>().points) >=0)){
+                instance = Instantiate(spawnee[1], spawnPoint.position, spawnPoint.rotation);
+                FollowThePath follow = instance.GetComponent(typeof(FollowThePath)) as FollowThePath;
+                //instance.name = "Ennemy";
+                follow.SetWaypoints(waypoints);
+                return spawnee[1].GetComponent<Ennemy>().points;
+            }else{
+                random += 0.5f;
+            }
+        }
+
+        if(random > 0.5){
+            if(((points -= spawnee[0].GetComponent<Ennemy>().points) >=0)){
+                instance = Instantiate(spawnee[0], spawnPoint.position, spawnPoint.rotation);
+                FollowThePath follow = instance.GetComponent(typeof(FollowThePath)) as FollowThePath;
+                //instance.name = "Ennemy";
+                follow.SetWaypoints(waypoints);
+                return spawnee[0].GetComponent<Ennemy>().points;
+            }
+        }
+        return 0;
     }
-
-
-    public int GetPoints(){
-        return this.points;
-    }
-
 }
+
